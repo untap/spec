c05bb4dbc03d8c3409ac1876c0af72c280c50ce5
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2017-10-09 17:31:31 +0000

    Allow optional filename for bin
    Allow commands such as `tap-format-spec <filename>`. This is handy if you are using piping through `xargs`.

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
+var fs = require('fs')
 var parser = require('@tap-format/parser')
 var formatAsSpec = require('../')
 
-var input$ = parser.observeStream(process.stdin)
+var stream = process.stdin
+
+if (process.argv[2]) {
+  stream = fs.createReadStream(process[argv[2]))
+}
+
+var input$ = parser.observeStream(stream)
 
 formatAsSpec(input$).forEach(console.log.bind(console))
 
